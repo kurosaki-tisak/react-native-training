6330fcde4491937ac94d551e2c81bca4259193b6
Kittisak Buaphanna <kurosaki_tisak@hotmail.com>
2018-10-08 22:45:40 +0700

    Add Save & Remove data from Firebase

--- manager/src/actions/EmployeeAction.js
+++ manager/src/actions/EmployeeAction.js
@@ -2,7 +2,8 @@ import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
 import {
     EMPLOYEE_UPDATE,
-    EMPLOYEE_FETCH_SUCESS
+    EMPLOYEE_FETCH_SUCCESS,
+    EMPLOYEE_SAVE_SUCCESS
 } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -18,18 +19,44 @@ export const employeeCreate = ({ name, phone, shift }) => {
     return () => {
         firebase.database().ref(`/users/${currentUser.uid}/employees`)
             .push({ name, phone, shift })
-            .then(() => Actions.employeeList({ type: 'reset' }) 
+            .then(() => Actions.employeeList({ type: 'reset' })
             );
     };
 };
 
 export const employeesFetch = () => {
-    const { currentUser } = firebase.auth()
+    const { currentUser } = firebase.auth();
 
     return (dispatch) => {
         firebase.database().ref(`/users/${currentUser.uid}/employees`)
             .on('value', snapshot => {
-                dispatch({ type: EMPLOYEE_FETCH_SUCESS, payload: snapshot.val() })
+                dispatch({ type: EMPLOYEE_FETCH_SUCCESS, payload: snapshot.val() })
+            });
+    };
+};
+
+export const employeeSave = ({ name, phone, shift, uid }) => {
+    const { currentUser } = firebase.auth();
+
+    return (dispatch) => {
+        firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
+            .set({ name, phone, shift })
+            .then(() => {
+                dispatch({ type: EMPLOYEE_SAVE_SUCCESS });
+                Actions.employeeList({ type: 'reset' });
+            }        
+            );
+    };
+};
+
+export const employeeDelete = ({ uid }) => {
+    const { currentUser } = firebase.auth();
+
+    return () => {
+        firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
+            .remove()
+            .then(() => {
+                Actions.employeeList({ type: 'reset' });
             });
     };
 };
--- manager/src/reducers/EmployeeReducer.js
+++ manager/src/reducers/EmployeeReducer.js
@@ -1,13 +1,16 @@
 import {
-    EMPLOYEE_FETCH_SUCESS
+    EMPLOYEE_FETCH_SUCCESS,
+    EMPLOYEE_SAVE_SUCCESS
 } from '../actions/types';
 
 const INITIAL_STATE = {};
 
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
-        case EMPLOYEE_FETCH_SUCESS:
+        case EMPLOYEE_FETCH_SUCCESS:
             return action.payload;
+        case EMPLOYEE_SAVE_SUCCESS:
+            return INITIAL_STATE;
         default:
             return state;
     }
